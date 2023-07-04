b34fefdc93a0dcdb626d8b5d39c05a85ee940f29
Muhammed AL-kohwaldeh <79379000+MASTAR-LAST@users.noreply.github.com>
2023-07-04 20:25:08 +0300

    make it available

--- voldemorts.sh
+++ voldemorts.sh
@@ -18,7 +18,6 @@ echo ""
 
 pip install cryptography
 pip install colorama
-sudo -H pip3 install cython
 
 echo ""
 echo ""
@@ -28,11 +27,7 @@ echo "Build the tool..."
 echo ""
 
 sudo chmod u+x voldemorts.py
-sudo cython3 voldemorts.py --embed -3
-sudo PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags)
-gcc -Os $(python3-config --includes) voldemorts.c -o voldemorts $(python3-config --ldflags) -l$PYTHONLIBVER
-sudo mv voldemorts /usr/local/bin
-sudo rm voldemorts.c
+sudo mv voldemorts.py /bin/voldemorts
 
 echo ""
 echo ""
