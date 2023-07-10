1ae90d45f9a6ad09cc757ecd2369028dcaa77483
Muhammed AL-kohwaldeh <79379000+MASTAR-LAST@users.noreply.github.com>
2023-07-10 16:09:45 +0300

    Updating build file

--- voldemorts.sh
+++ voldemorts.sh
@@ -3,7 +3,7 @@
 echo ""
 echo ""
 
-echo "Update The system"
+echo "Update The system..."
 
 echo ""
 
@@ -12,15 +12,23 @@ sudo apt update
 echo ""
 echo ""
 
-echo "Install packeges"
+echo "Install packeges..."
 
 echo ""
 
 pip install cryptography
 pip install colorama
 
+echo ""
+echo ""
+
+echo "Build the tool..."
+
+echo ""
+
 sudo chmod u+x voldemorts.py
-sudo mv voldemorts.py /usr/local/bin
+sudo mv voldemorts.py /bin/voldemorts
+sudo mv slayar.py /bin/voldemorts
 
 echo ""
 echo ""
