a4233547f5481137c6bcc6d0819fad807a37a7fa
MASTAR-LAST <belalalkohawaldeh@gmail.com>
2024-01-24 17:09:05 +0300

    Updating installer.sh file

--- installer.sh
+++ installer.sh
@@ -8,17 +8,29 @@
 echo ""
 echo ""
 
-echo "Today is " `date`
-echo "Update The system..."
+echo -e "Today is \033[44m" `date` + "\033[0m"
 
 echo ""
 
-sudo apt-get update
+echo -e "\033[1;93mUpdate The system...\033[0;39m"
+
+echo ""
+
+sudo apt-get update -y
 
 echo ""
 echo ""
 
-echo "Install packeges..."
+echo -e "\033[102mSources check...\033[0m"
+
+echo ""
+
+sudo apt-get install python3 python3-pip
+
+echo ""
+echo ""
+
+echo -e "\033[1;93mInstall necessary packages...\033[0;39m"
 
 echo ""
 
@@ -43,13 +55,21 @@ sudo apt-get install unzip
 echo ""
 echo ""
 
-echo "Build the tool..."
+echo -e "\033[1;93mBuild the tool...\033[0;39m"
+
+echo ""
+
+echo -e "\033[1;93mBuilding call command...\033[0;39m"
 
 echo ""
 
 sudo chmod u+x voldemorts.py
 sudo cp voldemorts.py /bin/voldemorts
 sudo mv voldemorts.py /usr/bin/voldemorts
+
+echo -e "\033[1;93mBuilding the helping script...\033[0;39m"
+
+echo ""
 
 sudo chmod u+x tool_updater25T.sh
 sudo cp tool_updater25T.sh /bin/tool_updater25T
@@ -58,7 +78,25 @@ sudo mv tool_updater25T.sh /usr/bin/tool_updater25T
 echo ""
 echo ""
 
-echo "Done !"
+echo -e "\033[1;93mBuilding the configuration file...\033[0;39m"
+
+echo ""
+
+sudo mv volde_info /usr/volde_info
+
+echo ""
+echo ""
+
+echo -e "\033[1;97mCleanning...\033[0m"
+
+echo ""
+
+sudo apt-get autoremove
+
+echo ""
+echo ""
+
+echo -e "\033[1;92mDone !\033[0m"
 
 echo ""
 
